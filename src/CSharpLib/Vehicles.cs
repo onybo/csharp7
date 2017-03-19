c64cd940745c179348c0f41f5ab4be95d22df434
on <olavnybo@gmail.com>
2017-03-19 12:00:05 +0100

    switch on types

---
+++ src/CSharpLib/Vehicles.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLib
+{
+    public class Person
+    {
+        public Person(int weight)
+        {
+            Weight = weight;
+        }
+
+        public int Weight { get; private set; }
+    };
+
+    public class Car
+    {
+        public Car(int weight, Person driver, IEnumerable<Person> passengers)
+        {
+            Weight = weight;
+            Driver = driver ?? throw new ArgumentNullException(nameof(driver));                
+            Passengers = passengers?.ToArray() ?? throw new ArgumentNullException(nameof(passengers));
+        }
+        public Person[] Passengers { get; private set; }
+        public Person Driver { get; private set; }
+        public int Weight { get; private set; }
+    }
+
+    public class SportsCar
+    {
+        public SportsCar(int weight, Person driver, Person passenger)
+        {
+            Weight = weight;
+            Driver = driver ?? throw new ArgumentNullException(nameof(driver));
+            Passenger = passenger ?? throw new ArgumentNullException(nameof(passenger));
+        }
+        public Person Passenger { get; private set; }
+        public Person Driver { get; private set; }
+        public int Weight { get; private set; }
+    }
+
+    public class Bike
+    {
+        public Bike(int weight, Person rider)
+        {
+            Weight = weight;
+            Rider = rider ?? throw new ArgumentNullException(nameof(rider));
+        }
+        public Person Rider { get; private set; }
+        public int Weight { get; private set; }
+    }
+
+    public class VehicleWeightCalculator
+    {
+        public static int GetTotalWeight(object vehicle)
+        {
+            switch (vehicle)
+            {
+                case Car car:
+                    return car.Weight + car.Driver.Weight + car.Passengers.Sum(p => p.Weight);
+                case SportsCar sc:
+                    return sc.Weight + sc.Driver.Weight + sc.Passenger.Weight;
+                case Bike b:
+                    return b.Weight + b.Rider.Weight;
+                case var x:
+                    throw new NotImplementedException($"dont know how to calculate weight of {x.GetType().Name}");
+            }
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CSharpLib;
+
+namespace csharp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bike = new Bike(10, new Person(80));
+            Console.WriteLine($"total weight of bike is: {VehicleWeightCalculator.GetTotalWeight(bike)}");
+
+            var car = new Car(1000, new Person(80), new[] { new Person(75) });
+            Console.WriteLine($"total weight of car is: {VehicleWeightCalculator.GetTotalWeight(car)}");
+
+            Console.ReadKey();
+        }
+    }
+}
